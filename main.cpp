9d4a3701d33578902c8a7a2e05f950b64f98dfa1
Niloy <mkniloy1568@gmail.com>
2024-02-22 00:38:12 +0600

    com

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout<<s<<programmer;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
